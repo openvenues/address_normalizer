5886a13e2ac6989b8408e84060f18a78670ea68f
Al <albarrentine@gmail.com>
2015-01-08 14:34:20 -0500

    [util] Lookup Enum by string value

--- address_normalizer/utils/enum.py
+++ address_normalizer/utils/enum.py
@@ -1,9 +1,9 @@
 
 
 class EnumValue(object):
-    def __init__(self, value, description):
+    def __init__(self, value, name):
         self.value = value
-        self.description = description
+        self.name = name
 
     def __hash__(self):
         return self.value
@@ -15,20 +15,22 @@ class EnumValue(object):
             return self.value.__cmp__(other)
 
     def __unicode__(self):
-        return self.description
+        return self.name
 
     def __str__(self):
-        return self.description
+        return self.name
 
     def __repr__(self):
-        return self.description
+        return self.name
 
 class EnumMeta(type):
     def __init__(cls, name, bases, dict_):
         cls.registry = cls.registry.copy()
+        cls.name_registry = cls.name_registry.copy()
         for k, v in dict_.iteritems():
             if isinstance(v, EnumValue) and v not in cls.registry:
                 cls.registry[v.value] = v
+                cls.name_registry[v.name] = v
         return super(EnumMeta, cls).__init__(name, bases, dict_)
 
     def __iter__(cls):
@@ -40,10 +42,18 @@ class EnumMeta(type):
 class Enum(object):
     __metaclass__ = EnumMeta
     registry = {}
+    name_registry = {}
 
     @classmethod
     def from_id(cls, value):
         try:
             return cls.registry[value]
         except KeyError:
-            raise ValueError('Invalid value for %s: %s' % cls.__name__, value)
+            raise ValueError('Invalid value for {}: {}'.format(cls.__name__, value))
+
+    @classmethod
+    def from_string(cls, name):
+        try:
+            return cls.name_registry[name]
+        except KeyError:
+            raise ValueError('Invalid name for {}: {}'.format(cls.__name__, name))
