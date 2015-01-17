99eb61735bb7b25a87c0291ffd1ceb494361ecd2
Al <albarrentine@gmail.com>
2015-01-17 11:44:32 -0500

    [fix] encoding module stringifies other values

--- address_normalizer/text/encoding.py
+++ address_normalizer/text/encoding.py
@@ -1,28 +1,34 @@
 import six
 
-def safe_decode(text, encoding='utf-8', errors='strict'):
-    if isinstance(text, six.text_type):
-        return text
-    
-    if isinstance(text, (six.string_types, six.binary_type)):
-        return text.decode(encoding, errors)    
-    else:
-        raise TypeError('{} cannot be decoded'.format(type(text)))
+text_type = six.text_type
+string_types = six.string_types
+binary_type = six.binary_type
 
-def safe_encode(text, incoming=None, encoding='utf-8', errors='strict'):
-    if not isinstance(text, (six.string_types, six.binary_type)):
-        raise TypeError('{} cannot be decoded'.format(type(text)))
+
+def safe_decode(value, encoding='utf-8', errors='strict'):
+    if isinstance(value, text_type):
+        return value
     
-    if isinstance(text, six.text_type):
-        return text.encode(encoding, errors)
+    if isinstance(value, (string_types, binary_type)):
+        return value.decode(encoding, errors)
+    else:
+        return binary_type(value).decode(encoding, errors)
+
+
+def safe_encode(value, incoming=None, encoding='utf-8', errors='strict'):
+    if not isinstance(value, (string_types, binary_type)):
+        return binary_type(value)
+    
+    if isinstance(value, text_type):
+        return value.encode(encoding, errors)
     else:
         if hasattr(incoming, 'lower'):
             incoming = incoming.lower()
         if hasattr(encoding, 'lower'):
             encoding = encoding.lower()
 
-        if text and encoding != incoming:
-            text = safe_decode(text, encoding, errors)
-            return text.encode(encoding, errors)
+        if value and encoding != incoming:
+            value = safe_decode(value, encoding, errors)
+            return value.encode(encoding, errors)
         else:
-            return text
+            return value
