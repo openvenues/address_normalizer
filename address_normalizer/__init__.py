843af556a05077bca3ae9f5a947f93d3a8f4729d
Al <albarrentine@gmail.com>
2015-01-17 11:43:48 -0500

    [api] pass keyword args to expand_* calls

--- address_normalizer/__init__.py
+++ address_normalizer/__init__.py
@@ -1,8 +1,10 @@
 from .text.normalize import address_phrase_filter
 
 
-def expand_full_address(address):
-    return address_phrase_filter.expand_full_address(address)
+# Valid kwargs: normalize_numex=True, to_ascii=False, remove_accents=True
+def expand_full_address(address, **kw):
+    return address_phrase_filter.expand_full_address(address, **kw)
 
-def expand_street_address(street_address):
-    return address_phrase_filter.expand_street_address(street_address)
+
+def expand_street_address(street_address, **kw):
+    return address_phrase_filter.expand_street_address(street_address, **kw)
