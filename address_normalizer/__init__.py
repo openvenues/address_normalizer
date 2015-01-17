575f1d3f5fc564431380d0f4a309fd8b6d93cb27
Al <albarrentine@gmail.com>
2015-01-17 16:22:57 -0500

    [api] stubs for normalize calls

--- address_normalizer/__init__.py
+++ address_normalizer/__init__.py
@@ -8,3 +8,12 @@ def expand_full_address(address, **kw):
 
 def expand_street_address(street_address, **kw):
     return address_phrase_filter.expand_street_address(street_address, **kw)
+
+
+# Stubs
+def normalize_street_address(street_address, **kw):
+    return list(expand_street_address(street_address, **kw))[0]
+
+
+def normalize_full_address(address, **kw):
+    return list(expand_full_address(address))[0]
